9a5f76ce969e345a101fd054e15305ac88b4ea68
Damian_Achata <damian_lh05@hotmail.com>
2022-12-14 19:56:54 -0300

    Function that return random value from array

--- myScript.js
+++ myScript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function computerChoice(choices) {
+    const randomValue = Math.floor(Math.random() * choices.length);
+
+    const item = choices[randomValue];
+
+    return item;
+
+    
+}
+const choices = ["rock","paper","scissors"];
+
+computerChoice(choices);
